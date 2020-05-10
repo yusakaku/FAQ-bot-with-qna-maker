51af9ecec5a303949b0f6470764f669ea0315afc
yusakaku <62639597+yusakaku@users.noreply.github.com>
2020-05-10 22:48:52 +0900

    Add files via upload

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+using System.Linq;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+
+    public class EchoBot : ActivityHandler
+    {
+
+        public QnAMaker EchoBotQnA { get; private set; }
+        public EchoBot(QnAMakerEndpoint endpoint)
+        {
+            // connects to QnA Maker endpoint for each turn
+            EchoBotQnA = new QnAMaker(endpoint);
+        }
+
+        private async Task AccessQnAMaker(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var results = await EchoBotQnA.GetAnswersAsync(turnContext);
+            if (results.Any())
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("QnA Maker Returned: " + results.First().Answer), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Sorry, could not find an answer in the Q and A system."), cancellationToken);
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            //await turnContext.SendActivityAsync(MessageFactory.Text($"‚²Ž¿–â‚ðŒJ‚è•Ô‚µ‚Ü‚·: {turnContext.Activity.Text}"), cancellationToken);
+
+            await AccessQnAMaker(turnContext, cancellationToken);
+        }
+
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
